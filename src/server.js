86285dde5fea1ee3c53a0348271070f502f87f2e
DamianYongjae <klick214@gmail.com>
2020-06-29 14:28:57 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,66 @@
+import "../.env";
+import cors from "cors";
+import express from "express";
+import nodemailer from "nodemailer";
+import nodemailerSendgrid from "nodemailer-sendgrid";
+
+const PORT = process.env.PORT || 4000;
+
+const UNIX_Timestamp = 1575909015;
+
+export const sendMailNew = (email) => {
+  const transport = nodemailer.createTransport(
+    nodemailerSendgrid({
+      apiKey: process.env.SENDGRID_API_KEY,
+    }),
+  );
+
+  return transport.sendMail(email, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+export const sendScheduledMail = (address, subject, content) => {
+  let date = Math.round(new Date("June 29, 2020 12:37:00").getTime() / 1000);
+  const email = {
+    from: "CBLM@CBLM.com",
+    to: address,
+    subject: subject,
+    html: `기도 지향 내용: <p>${content}</p>`,
+    send_at: date,
+  };
+  return sendMailNew(email);
+};
+
+const server = express();
+server.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }),
+  express.json(),
+);
+
+server.post(`/sendmail`, (req, res) => {
+  try {
+    let data = {
+      address: req.body.email,
+      subject: "26 차 요한연수 지향",
+      content: req.body.intention,
+    };
+    sendScheduledMail(data.address, data.subject, data.content);
+    res.end();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+server.listen(
+  { port: PORT },
+  () => console.log(`Server running on http://localhost:${PORT}`),
+);
