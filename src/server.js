dc73c3bc567d3ff894df4122e4099dcbe2be6626
DamianYongjae <klick214@gmail.com>
2020-06-29 21:52:56 -0400

    res

--- src/server.js
+++ src/server.js
@@ -35,6 +35,12 @@ export const sendScheduledMail = (address, subject, content) => {
 const server = express();
 server.use(cors());
 
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", *); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 server.post(`/sendmail`, cors(), (req, res) => {
   try {
     let data = {
@@ -49,6 +55,6 @@ server.post(`/sendmail`, cors(), (req, res) => {
   }
 });
 
-server.listen(4000, function () {
-  console.log("app is listening");
-});
+// server.listen(4000, function () {
+//   console.log("app is listening");
+// });
