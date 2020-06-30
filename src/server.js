8d06652f52e39e3fe5ff9f1d54fc787be4994e42
DamianYongjae <klick214@gmail.com>
2020-06-29 21:59:03 -0400

    header

--- src/server.js
+++ src/server.js
@@ -40,6 +40,10 @@ server.use(function (req, res, next) {
     "Access-Control-Allow-Origin",
     "https://stoic-mccarthy-046543.netlify.app"
   ); // update to match the domain you will make the request from
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
