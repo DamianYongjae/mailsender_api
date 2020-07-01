31998fe753b40c0067e6bc85b5bc8447f1e2509c
DamianYongjae <klick214@gmail.com>
2020-07-01 13:44:23 -0400

    remove

--- src/server.js
+++ src/server.js
@@ -15,24 +15,24 @@ server.use(
 );
 server.use(express.json({ type: ["application/json", "text/plain"] })); // for parsing application/json
 server.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "*",
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization",
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "*",
+//   ); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization",
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
-server.options("*", cors());
+// server.options("*", cors());
 
 server.use("/", router);
 
