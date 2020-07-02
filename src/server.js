a3c42ec3c0f347b46953a89a7df2cf097ea6d046
DamianYongjae <klick214@gmail.com>
2020-07-02 16:28:38 -0400

    remove server.post

--- src/server.js
+++ src/server.js
@@ -62,17 +62,17 @@ router.post(
   sendmail
 );
 
-server.post(
-  "/sendmail",
-  cors({
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
-    origin: "*",
-    credentials: true,
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }),
-  sendmail
-);
+// server.post(
+//   "/sendmail",
+//   cors({
+//     methods: ["GET", "POST", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
+//     origin: "*",
+//     credentials: true,
+//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }),
+//   sendmail
+// );
 
 server.use(router);
 
