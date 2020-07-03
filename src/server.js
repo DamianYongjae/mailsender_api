f5ff7244d51c83d0616d75e9d8669d2cc26ca37e
DamianYongjae <klick214@gmail.com>
2020-07-03 14:33:20 -0400

    changed to cors

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ import { sendmail } from "./sendmail";
 const router = express.Router();
 
 const server = express();
-// server.use(
-//   cors({
-//     methods: ["GET", "POST", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
-//     origin: "*",
-//     credentials: true,
-//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   })
-// );
+server.use(
+  cors({
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 server.use(express.json({ type: ["application/json"] })); // for parsing application/json
 server.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -33,20 +33,20 @@ server.use(express.urlencoded({ extended: true })); // for parsing application/x
 //   return await fn(req, res)
 // }
 
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+// server.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 // server.enable("trust proxy");
 
