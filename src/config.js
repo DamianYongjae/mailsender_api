b0eb61697a72ff927e6b945aeef247a78a9b7615
DamianYongjae <klick214@gmail.com>
2020-07-03 16:59:23 -0400

    add routes

--- src/config.js
+++ src/config.js
@@ -4,23 +4,6 @@ import "../.env";
 
 dotenv.config();
 
-export const sendMailNew = (email) => {
-  const transport = nodemailer.createTransport(
-    nodemailerSendgrid({
-      apiKey: process.env.SENDGRID_API_KEY,
-    })
-  );
-
-  return transport.sendMail(email, (res, err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(res);
-      res.end();
-    }
-  });
-};
-
 export const sendScheduledMail = (address, subject, content) => {
   let date = Math.round(new Date("June 29, 2020 12:37:00").getTime() / 1000);
   let tempDate = Math.round(new Date().getTime() / 1000);
