7f51d80e6ba6349161dce69c77ab98e4f4b24b1f
DamianYongjae <klick214@gmail.com>
2020-07-03 21:17:39 -0400

    cors handler

---
+++ src/cors-handler.ts
@@ -0,0 +1,6 @@
+export default async function (req: any, res: any) {
+  // There's nothing to do here except to end the response since CORS headers
+  // are automatically added for all routes using the `now.json` file.
+  console.log("options handler being called");
+  return res.status(204).send();
+}
