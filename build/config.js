83abd479a2a09130f979af858a4151ce9a071d7e
DamianYongjae <klick214@gmail.com>
2020-07-04 07:52:41 -0400

    script

--- build/config.js
+++ build/config.js
@@ -1,1 +1,34 @@
 "use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sendScheduledMail = void 0;
+
+var _mail = _interopRequireDefault(require("@sendgrid/mail"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+require("../.env");
+
+_dotenv["default"].config();
+
+var sendScheduledMail = function sendScheduledMail(address, subject, content) {
+  var date = Math.round(new Date("June 29, 2020 12:37:00").getTime() / 1000);
+  var tempDate = Math.round(new Date().getTime() / 1000);
+  var email = {
+    from: "CBLM@CBLM.com",
+    to: address,
+    subject: subject,
+    html: "\uAE30\uB3C4 \uC9C0\uD5A5 \uB0B4\uC6A9: <p>".concat(content, "</p>"),
+    send_at: tempDate
+  };
+
+  _mail["default"].setApiKey(process.env.SENDGRID_API_KEY);
+
+  return _mail["default"].send(email);
+};
+
+exports.sendScheduledMail = sendScheduledMail;
--- build/server.js
+++ build/server.js
@@ -5,72 +5,45 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.sendScheduledMail = exports.sendMailNew = void 0;
-
-require("../.env");
+exports["default"] = exports.router = void 0;
 
 var _cors = _interopRequireDefault(require("cors"));
 
 var _express = _interopRequireDefault(require("express"));
 
-var _nodemailer = _interopRequireDefault(require("nodemailer"));
+var _sendmail = require("./sendmail");
 
-var _nodemailerSendgrid = _interopRequireDefault(require("nodemailer-sendgrid"));
+var _corsHandler = _interopRequireDefault(require("./cors-handler"));
 
-var PORT = process.env.PORT || 4000;
-var UNIX_Timestamp = 1575909015;
-
-var sendMailNew = function sendMailNew(email) {
-  var transport = _nodemailer["default"].createTransport((0, _nodemailerSendgrid["default"])({
-    apiKey: process.env.SENDGRID_API_KEY
-  }));
-
-  return transport.sendMail(email, function (err, info) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
-  });
-};
-
-exports.sendMailNew = sendMailNew;
-
-var sendScheduledMail = function sendScheduledMail(address, subject, content) {
-  var date = Math.round(new Date("June 29, 2020 12:37:00").getTime() / 1000);
-  var email = {
-    from: "CBLM@CBLM.com",
-    to: address,
-    subject: subject,
-    html: "\uAE30\uB3C4 \uC9C0\uD5A5 \uB0B4\uC6A9: <p>".concat(content, "</p>"),
-    send_at: date
-  };
-  return sendMailNew(email);
-};
-
-exports.sendScheduledMail = sendScheduledMail;
 var server = (0, _express["default"])();
 server.use((0, _cors["default"])({
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
   origin: "*",
   credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 
-}), _express["default"].json());
-server.post("/sendmail", function (req, res) {
-  try {
-    var data = {
-      address: req.body.email,
-      subject: "26 차 요한연수 지향",
-      content: req.body.intention
-    };
-    sendScheduledMail(data.address, data.subject, data.content);
-    res.end();
-  } catch (error) {
-    res.status(500).send();
-  }
+}));
+server.use(_express["default"].json({
+  type: ["application/json"]
+})); // for parsing application/json
+
+server.use(_express["default"].urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+var router = _express["default"].Router();
+
+exports.router = router;
+router.post("/sendmail", (0, _cors["default"])(), _sendmail.sendmail);
+router.options("*", (0, _cors["default"])({
+  origin: "*",
+  credentials: true,
+  optionsSuccessStatus: 200
+}), _corsHandler["default"]);
+server.use(router);
+server.listen(4000, function () {
+  console.log("app is listening");
 });
-server.listen({
-  port: PORT
-}, function () {
-  return console.log("Server running on http://localhost:".concat(PORT));
-});
+var _default = server;
+exports["default"] = _default;
