ac6d517ae00a9722f76810b8bf59f64add1ea95d
DamianYongjae <klick214@gmail.com>
2020-07-15 15:15:47 -0400

    change send date

--- src/config.js
+++ src/config.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sendScheduledMail = (address, subject, content) => {
-  let date = Math.round(new Date("June 29, 2020 12:37:00").getTime() / 1000);
+  let date = Math.round(new Date("August 30, 2020 18:00:00").getTime() / 1000);
   let tempDate = Math.round(new Date().getTime() / 1000);
   const email = {
     from: "CBLM@CBLM.com",
     to: address,
     subject: subject,
     html: content,
-    send_at: tempDate,
+    send_at: date,
   };
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
