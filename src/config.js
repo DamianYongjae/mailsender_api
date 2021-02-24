9e1df3f3fe643330e59f965d879bed29a4833610
DamianYongjae <klick214@gmail.com>
2021-02-24 14:31:30 -0500

    fix

--- src/config.js
+++ src/config.js
@@ -7,8 +7,8 @@ export const sendScheduledMail = (address, subject, content) => {
   let date = Math.round(new Date("August 30, 2020 18:00:00").getTime() / 1000);
   let tempDate = Math.round(new Date().getTime() / 1000);
   let now = new Date();
-  let time = date.setMinutes(now.getMinutes() + 5);
-  let sendTime = date.setDate(now.getDate() + 2);
+  let time = Math.round(date.setMinutes(now.getMinutes() + 5).getTime() / 1000);
+  let sendTime = Math.round(date.setDate(now.getDate() + 2).getTime() / 1000);
   const email = {
     from: "CBLM@CBLM.com",
     to: address,
