d2b3182afafb470cb3fd8a8476f0555be116c7c5
DamianYongjae <klick214@gmail.com>
2021-02-24 14:25:38 -0500

    5min

--- src/config.js
+++ src/config.js
@@ -6,12 +6,15 @@ dotenv.config();
 export const sendScheduledMail = (address, subject, content) => {
   let date = Math.round(new Date("August 30, 2020 18:00:00").getTime() / 1000);
   let tempDate = Math.round(new Date().getTime() / 1000);
+  let now = new Date();
+  let time = date.setMinutes(now.getMinutes() + 5);
+  let sendTime = date.setDate(now.getDate() + 2);
   const email = {
     from: "CBLM@CBLM.com",
     to: address,
     subject: subject,
     html: content,
-    send_at: tempDate,
+    send_at: time,
   };
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
