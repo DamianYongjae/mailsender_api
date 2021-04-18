a17cbb4b7831940eecfe29d6b5665933c98c5cd7
DamianYongjae <klick214@gmail.com>
2021-04-18 16:23:46 -0400

    sendtime test

--- src/config.js
+++ src/config.js
@@ -9,25 +9,25 @@ export const sendScheduledMail = (address, subject, content) => {
   let now1 = new Date();
   let now2 = new Date();
   // let time = Math.round(now1.setMinutes(now1.getMinutes() + 5) / 1000);
-  // let sendTime = Math.round(now2.setDate(now2.getDate() + 2) / 1000);
+  let sendTime = Math.round(now2.setDate(now2.getDate() + 2) / 1000);
   const email = {
     from: "CBLM@CBLM.com",
     to: address,
     subject: subject,
     html: content,
-    send_at: Math.round(now1.setMinutes(now1.getMinutes() + 5) / 1000),
-    // send_at: tempDate,
+    // send_at: Math.round(now1.setMinutes(now1.getMinutes() + 5) / 1000),
+    send_at: sendTime,
   };
   const email2 = {
     from: "CBLM@CBLM.com",
     to: "cblmne@gmail.com",
     subject: subject,
     html: content,
-    send_at: Math.round(now2.setMinutes(now2.getMinutes() + 5) / 1000),
-    // send_at: tempDate,
+    // send_at: Math.round(now2.setMinutes(now2.getMinutes() + 5) / 1000),
+    send_at: sendTime,
   };
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.send(email2);
+  // sgMail.send(email2);
   return sgMail.send(email);
 };
